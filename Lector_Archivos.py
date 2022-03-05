01f51f8a87cb8a6b45bfca7f810de7455105c2e4
alefrarod <alefrarod@alum.us.es>
2022-03-05 17:29:14 +0100

    añadido nuevas funcionalidades del PAI1

--- Lector_Archivos.py
+++ Lector_Archivos.py
@@ -1,6 +1,7 @@
 #Lector de archivos
 
 import hashlib
+import os
 
 sha = hashlib.sha256()
 
@@ -15,5 +16,48 @@ def hash_file(filename):
 
     return sha.hexdigest()
 
-message = hash_file("C:\\Users\\alefr\\pyHIDS\\archivos\\c.txt")
-print(message)
+#message = hash_file("C:\\Users\\alefr\\pyHIDS\\archivos\\c.txt")
+#print(message)
+
+
+def escritura(filename):
+    ruta ='./archivos'
+    contenido = os.listdir(ruta)
+    for fichero in contenido:
+        hash=hash_file(ruta+'/'+fichero)
+        archivo= open(filename, 'a')
+        archivo.writelines(fichero+':'+hash+'\n')
+        archivo.close() 
+         
+    archivo= open(filename, 'r')
+    print(archivo.read())  
+    archivo.close() 
+
+
+
+
+def comp_hash(filename, almacenamiento):
+    ruta=filename.split('/')
+    nombre=ruta[-1]
+    hash=hash_file(filename)
+    archivo=open(almacenamiento, 'r')
+    lineas=archivo.readlines()
+    lista=[]  
+    lista_hash=[]
+    for linea in lineas:
+        trozos=linea.split(':')
+        lista.append(trozos[0])
+        lista_hash.append(trozos[1])
+        
+    if nombre in lista:
+        if not (hash in lista_hash):
+            print("Se ha detectado un fallo de integridad en el archivo "+nombre)
+
+    else:
+        with open(almacenamiento, 'a') as file:
+            file.writelines(nombre+':'+hash+'\n')
+
+
+#escritura(".\\almacenamiento\\prueba.txt")
+
+comp_hash('./archivos/a.txt', './almacenamiento/prueba.txt')
